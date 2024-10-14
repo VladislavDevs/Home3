ec5ab5ed34b08b0a0199ef1f8dbe0ed1567a9a37
VladislavDevs <supermars5327@gmail.com>
2024-10-15 00:43:09 +0700

    Выполненное домашнее задание

---
+++ DictionaryKeysCount.py
@@ -0,0 +1,18 @@
+items = {
+ 'milk15':{'name': 'молоко 1.5%', 'count': 34, 'price': 89.9},
+ 'cheese':{'name': 'сыр молочный 1 кг.', 'count': 12, 'price': 990.9},
+ 'sausage':{'name': 'колбаса 1 кг.', 'count': 122, 'price': 1990.9}
+}
+
+def CountMore20(items : dict, key : str):
+ if(items[key]['count'] < 20):
+  return True
+ else:
+  return False
+
+resultList = {}
+
+for key in items:
+ resultList[key] = CountMore20(items, key)
+
+print(resultList)
